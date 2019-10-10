57f3717d9a120996db458349d4acb063b88a1cf9
touhid-jisan <touhidulislamjisan@gmail.com>
2019-10-10 21:46:05 +0600

    Binary Tree

---
+++ DatastructureAndAlgorithms/src/datastructure/tree/practice1/BinaryTree.java
@@ -0,0 +1,46 @@
+package datastructure.tree.practice1;
+
+public class BinaryTree {
+	Node root;
+	
+	public void addNode(int key, String name) {
+		Node new_Node = new Node(key, name);
+		
+		if(root == null) {
+			root = new_Node;
+		}
+		else {
+			Node focusNode = root;
+			Node parent;
+			
+			while(true) {
+				parent = focusNode;
+				if(key < focusNode.key) {
+					focusNode = focusNode.leftChild;
+					
+					if(focusNode == null) {
+						parent.leftChild = new_Node;
+						return;
+					}
+				}
+				else {
+					focusNode = focusNode.rightChild;
+					if(focusNode == null) {
+						parent.rightChild = new_Node;
+						return;
+					}
+				}
+			}
+		}
+		
+		
+	}
+	public void showBinaryTree(Node focusNode) {
+		if(focusNode != null) {
+			
+			showBinaryTree(focusNode.leftChild);
+			System.out.println(focusNode);
+			showBinaryTree(focusNode.rightChild);
+		}
+	}
+}
---
+++ DatastructureAndAlgorithms/src/datastructure/tree/practice1/Main.java
@@ -0,0 +1,16 @@
+package datastructure.tree.practice1;
+
+public class Main {
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+		tree.addNode(1, "jisan");
+		tree.addNode(15, "jisan");
+		tree.addNode(17, "jisan");
+		tree.addNode(21, "jisan");
+		tree.addNode(134, "jisan");
+		tree.addNode(14, "jiafasan");
+		tree.addNode(17, "jisan");
+		
+		tree.showBinaryTree(tree.root);
+	}
+}
